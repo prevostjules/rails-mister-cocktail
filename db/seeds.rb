cf60851d660da90b43c384c4541fc5388d13a3c4
Jules Prevost <jprevostdev@gmail.com>
2020-06-11 15:54:07 +0200

    all routes work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy current cocktails in DB"
+
+Cocktail.destroy_all
+
+puts "Destroy current ingredients in DB"
+
+Ingredient.destroy_all
+Dose.destroy_all
+
+
+puts "Create ingredients"
+
+a = Ingredient.new(name: "lemon")
+a.save!
+b = Ingredient.new(name: "ice")
+b.save!
+c = Ingredient.new(name: "mint leaves")
+c.save!
+
+
+
+mary = Cocktail.new(name: "Bloody Mary")
+mary.save!
+gr = Dose.new(description: "3 grammes")
+gr.ingredient = a
+gr.cocktail = mary
+gr.save!
+
+henry = Cocktail.new(name: "Bloody Henry")
+henry.save!
+feuilles = Dose.new(description: "3 feuilles")
+feuilles.ingredient = b
+feuilles.cocktail = henry
+feuilles.save!
+
+george = Cocktail.create(name: "Bloody George")
+george.save!
+pounds = Dose.new(description: "3 pounds")
+pounds.ingredient = b
+pounds.cocktail = george
+pounds.save!
+
+
+puts "Finished"
